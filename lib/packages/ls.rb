57f24eb204f41663b416f3807af30d4a62ca1090
Syed Faraaz Ahmad <faraaz98@live.com>
2017-02-19 14:40:13 +0530

    Add class to dev

--- lib/packages/ls.rb
+++ lib/packages/ls.rb
@@ -1,23 +1,27 @@
-def show_all
-  Dir.entries(Dir.pwd).each do |entry|
-    print "#{entry}\t"
-  end
-  print "\n"
-end
+class Ls
 
-def show_non_hidden
-  Dir.entries(Dir.pwd).each do |entry|
-    print "#{entry}\t" unless entry[0] == "."
-  end
-end
-
-def ls(params)
-  if params.length == 0
-    show_non_hidden
-  else
-    case params[0]
-      when "-a"
-        show_all
+  def initialize(params)
+    if params.length == 0
+      show_non_hidden
+    else
+      case params[0]
+        when "-a"
+          show_all
+      end
     end
   end
+
+  def show_all
+    Dir.entries(Dir.pwd).each do |entry|
+      print "#{entry}\t"
+    end
+    print "\n"
+  end
+
+  def show_non_hidden
+    Dir.entries(Dir.pwd).each do |entry|
+      print "#{entry}\t" unless entry[0] == "."
+    end
+  end
+
 end
