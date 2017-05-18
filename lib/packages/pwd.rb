e168fd2798d3a04a24f4a6f08c159ee7f4570b25
Syed Faraaz Ahmad <faraaz98@live.com>
2017-05-19 03:44:46 +0530

    turn pwd into class

--- lib/packages/pwd.rb
+++ lib/packages/pwd.rb
@@ -1,3 +1,15 @@
-def pwd(params)
-  puts Dir.pwd
+require "set"
+require_relative "core"
+
+class Pwd
+
+	def self.main params
+		result = Set.new
+		result.add Dir.pwd
+		
+		Core.print_result result
+	end
+
 end
+
+
