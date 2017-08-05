c53a60365e0e863ed4eff91ed96a408c5961da28
Syed Faraaz Ahmad <faraaz98@live.com>
2017-08-05 15:52:38 +0530

    Update app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -18,7 +18,7 @@ $hist_file = init_history('.srb_history')
 loop do
   display_prompt
   raw_input = gets
-  handle_commands(:Exit,nil) if raw_input.nil?
+  handle_commands(:Exit, nil) if raw_input.nil?
   $hist_file.puts raw_input
   input = raw_input.gsub("\n", "").split(" ")
     if input[0] != ""
