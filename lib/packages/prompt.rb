97956f814d456533961f2cdbcf26fe3c8fc2491e
Jacob Herrington <jacobherrington@collectivebias.com>
2017-08-16 15:13:34 -0500

    add os gem update prompt to show username

--- lib/packages/prompt.rb
+++ lib/packages/prompt.rb
@@ -1,6 +1,8 @@
 require 'colorized_string'
+require 'os'
 
 def display_prompt
+  print ColorizedString[(OS.windows? ? ENV["USERNAME"] : ENV["USER"]) + "@"].colorize(:yellow)
   print ColorizedString["SRB "].colorize(:yellow)
   print ColorizedString["#{Dir.pwd}"].colorize(:blue)
   print "> "
