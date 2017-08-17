1de2c3f3cdcb9c10e9ae71374ca556fb97c691e9
Jacob Herrington <jacobherrington@collectivebias.com>
2017-08-17 07:53:27 -0500

    wip

--- lib/packages/mkdir.rb
+++ lib/packages/mkdir.rb
@@ -4,6 +4,7 @@ require_relative "core"
 class Mkdir 
 
   def self.main params
+    flags = params.flat_map{ |s| s.scan(/--?([^=\s]+)(?:=(\S+))?/) }
     params.each do |directory_name|
       Dir.mkdir(directory_name) unless File.exists?(directory_name)
     end
