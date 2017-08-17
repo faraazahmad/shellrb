265b2b4ca63df74def1ee4ab091d4a45b843bd14
jacobherrington <jherrington.personal@gmail.com>
2017-08-17 10:36:16 -0500

    add spec for mkdir

---
+++ spec/packages/mkdir_spec.rb
@@ -0,0 +1,19 @@
+require "packages/cd"
+
+describe Mkdir do
+
+  describe ".main" do
+    context "given an existing directory" do
+      it "doesn't make a new directory" do
+        #todo
+      end
+    end
+
+    context "given an new directory" do
+      it "makes a new directory" do
+        #todo
+      end
+    end
+  end
+
+end
