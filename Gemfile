4e832281f3e014468d05e6071031e61ca05d7828
jacob <jherrington.personal@gmail.com>
2017-08-21 16:11:53 -0500

    add os gem update prompt to show username (#16)
    * add os gem update prompt to show username
    
    * create get_user method
    
    * style changes
    
    * rename get_user to display_user

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'psych'
 gem 'colorize'
+gem 'os'
 
 group :test, :development do
   gem 'rspec'
--- lib/packages/prompt.rb
+++ lib/packages/prompt.rb
@@ -1,7 +1,13 @@
 require 'colorized_string'
+require 'os'
 
 def display_prompt
+  print ColorizedString[display_user].colorize(:yellow)
   print ColorizedString["SRB "].colorize(:yellow)
   print ColorizedString["#{Dir.pwd}"].colorize(:blue)
   print "> "
+end
+
+def display_user
+  (OS.windows? ? ENV['USERNAME'] : ENV['USER']) + '@'
 end
