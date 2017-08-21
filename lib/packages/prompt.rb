c13ee969ba9bdd229c918ed95ff68836137af436
jacobherrington <jherrington.personal@gmail.com>
2017-08-21 13:40:59 -0500

    create get_user method

--- lib/packages/prompt.rb
+++ lib/packages/prompt.rb
@@ -2,8 +2,12 @@ require 'colorized_string'
 require 'os'
 
 def display_prompt
-  print ColorizedString[(OS.windows? ? ENV["USERNAME"] : ENV["USER"]) + "@"].colorize(:yellow)
+  print ColorizedString[get_user].colorize(:yellow)
   print ColorizedString["SRB "].colorize(:yellow)
   print ColorizedString["#{Dir.pwd}"].colorize(:blue)
   print "> "
+end
+
+def get_user
+  (OS.windows? ? ENV["USERNAME"] : ENV["USER"]) + "@"
 end
