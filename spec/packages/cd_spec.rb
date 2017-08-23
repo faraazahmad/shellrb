1dcc6a1905c5d2838ae65560c65d0160e869680b
toshokan <toshokan@users.noreply.github.com>
2017-08-23 15:50:51 -0400

    Fixes #6 (#9)

--- spec/packages/cd_spec.rb
+++ spec/packages/cd_spec.rb
@@ -11,4 +11,24 @@ describe Cd do
     end
   end
 
+  describe ".main" do
+    context "given non-existent directory" do
+      it "does not change directory" do
+        old_path = Dir.pwd
+        Cd.main(["does-not-exist"])
+        expect(Dir.pwd).to eql(old_path)
+      end
+    end
+  end
+
+  describe ".main" do
+    context "given a file" do
+      it "does not change directory" do
+        old_path = Dir.pwd
+        Cd.main(["lib/packages/cd.rb"])
+        expect(Dir.pwd).to eql(old_path)
+      end
+    end
+  end
+
 end
