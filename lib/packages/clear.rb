8f6f9ac510bf15cca1dc6530fa1769c19c85d0e3
jacob <jherrington.personal@gmail.com>
2017-08-31 12:34:02 -0500

    Build package for clear command (#20)
    * add clear.rb
    
    * remove unused require
    
    * add clear.rb
    
    remove unused require
    
    * post PR revision
    
    * add blank line for magic comment
    
    * more revisions

---
+++ lib/packages/clear.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'core'
+require 'os'
+
+# This class is used to clear the terminal
+class Clear
+  def self.main
+    OS.windows? ? system('cls') : system('clear')
+  end
+end
---
+++ spec/packages/clear_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'packages/clear'
+
+describe Clear do
+  context 'TODO - Write tests for packages/clear.rb'
+end
