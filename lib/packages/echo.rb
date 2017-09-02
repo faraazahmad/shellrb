bd5dd0427a6f156800fa3ff01473e84f936a5dd5
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-09-02 15:41:38 -0400

    Fix hound violations

--- lib/packages/echo.rb
+++ lib/packages/echo.rb
@@ -1,10 +1,10 @@
-#A simple echo command for shellrb
+# A simple echo command for shellrb
 class Echo
-  def self.main params
+  def self.main (params)
     if params[0] == '-n'
-      print params[1..(params.length - 1)].join(" ")
+      print params[1..(params.length - 1)].join(' ')
     else
-      puts params[0..(params.length - 1)].join(" ")
+      puts params[0..(params.length - 1)].join(' ')
     end
   end
 end
