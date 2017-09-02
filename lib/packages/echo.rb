8ac3c3c1d799eec728a7d1e03a9d320be4a34230
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-09-02 14:30:49 -0400

    Fix whitespace

--- lib/packages/echo.rb
+++ lib/packages/echo.rb
@@ -1,10 +1,10 @@
+#A simple echo command for shellrb
 class Echo
-	def self.main params
-		if params[0] == '-n'
-			print params[1..(params.length - 1)].join(" ")
-		else
-			puts params[0..(params.length - 1)].join(" ")
-		end
-	end
+  def self.main params
+    if params[0] == '-n'
+      print params[1..(params.length - 1)].join(" ")
+	else
+	  puts params[0..(params.length - 1)].join(" ")
+    end
+  end
 end
-
