477f5c1ef739cb7a1ff517e8803594730f514def
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-09-03 10:21:02 -0400

    Make requested fixes

--- lib/packages/echo.rb
+++ lib/packages/echo.rb
@@ -1,10 +1,20 @@
 # A simple echo command for shellrb
 class Echo
-  def self.main (params)
-    if params[0] == '-n'
-      print params[1..(params.length - 1)].join(' ')
-    else
-      puts params[0..(params.length - 1)].join(' ')
+  def self.main params
+    # 'finish' is the index of last item in the params entered by the user
+    finish = params.length - 1
+	case params[0]
+      # checks for the option '-n' which prints text without a newline
+      when '-n'
+		start = 1
+		# prints each item from index start (1 here to avoid printing -n) to finish
+		# .join(' ') joins the members of params with a space in between them to emulate a string
+		print params[start..finish].join(' ')
+	  else
+        start = 0
+		# does the same as above, but prints a newline at the end
+		# starts from index 0
+        puts params[start..finish].join(' ')
     end
   end
 end
