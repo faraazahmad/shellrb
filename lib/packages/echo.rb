c832ffee091060676e0c5d91a61102c896905555
Parker Knight <parkerknight@Parkers-MacBook-Air.local>
2017-09-07 20:24:10 -0400

    fetch upstream

--- lib/packages/echo.rb
+++ lib/packages/echo.rb
@@ -3,17 +3,17 @@ class Echo
   def self.main params
     # 'finish' is the index of last item in the params entered by the user
     finish = params.length - 1
-	case params[0]
+    case params[0]
       # checks for the option '-n' which prints text without a newline
       when '-n'
-		start = 1
-		# prints each item from index start (1 here to avoid printing -n) to finish
-		# .join(' ') joins the members of params with a space in between them to emulate a string
-		print params[start..finish].join(' ')
-	  else
+        start = 1
+        # prints each item from index start (1 here to avoid printing -n) to finish
+        # .join(' ') joins the members of params with a space in between them to emulate a string
+        print params[start..finish].join(' ')
+      else
         start = 0
-		# does the same as above, but prints a newline at the end
-		# starts from index 0
+        # does the same as above, but prints a newline at the end
+        # starts from index 0
         puts params[start..finish].join(' ')
     end
   end
