2ab07849c083758aa65680a77a8cea837b75df77
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-17 12:33:11 -0800

    refactor spec for Help

--- lib/packages/help.rb
+++ lib/packages/help.rb
@@ -2,17 +2,31 @@ require_relative 'core'
 require 'set'
 
 class Help
-  #outputs available methods for the user
+  # outputs available methods for the user
   def self.main params
     result = Set.new
     # Create new instance of Ruby data structure Set to add output to it
     # For more info on set: http://ruby-doc.org/stdlib-2.4.1/libdoc/set/rdoc/Set.html
-    entries = Dir.entries('lib/packages')
-    # formatted_entries = entries.map(&:strip_rb)
-    entries.each do |entry|
-      entry.include?('.rb') ? result.add(entry[0..-4]) : result.add(entry)
-    end
+    
+    # TODO: Handle case when params are sent as input
+
+    # Assuming no params have been sent
+
+    result.merge self.no_params
+
+    puts 'These commands are defined in lib/packages. Type \'help\' to see this list '
+    # Use the Core class of ShellRB to print result
     Core.print_result result
   end
 
+  def self.no_params
+    simple_result = Set.new
+    entries = Dir.entries('lib/packages')
+    
+    entries.each do |entry|
+      simple_result.add(entry[0..-4]) if entry[-3..-1] == '.rb'
+    end
+
+    simple_result
+  end
 end
--- spec/packages/help_spec.rb
+++ spec/packages/help_spec.rb
@@ -5,9 +5,7 @@ describe Help do
     describe '.main' do
         context 'when command is executed' do
             it 'shows all currently available functions' do
-                # lib_files = Dir.entries('../../lib/packages')
-                # commands = lib_files.map{|command| command.include?('rb') ? command[0..-4] : command}
-                expect(Help.main('').to output('').to_stdout
+                expect(Help.main('')).to eql(Help.no_params)
             end
         end
     end
