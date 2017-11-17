689f8da2807f98a16c2b6d482e0050a4f2cb266b
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-17 12:17:49 -0800

    refactor help class

--- spec/packages/help_spec.rb
+++ spec/packages/help_spec.rb
@@ -5,23 +5,9 @@ describe Help do
     describe '.main' do
         context 'when command is executed' do
             it 'shows all currently available functions' do
-                lib_files = Dir.entries('../../lib/packages')
-                commands = lib_files.map{|command| command.include?('rb') ? command[0..-4] : command}
-                expect(Help.main('').to output(<<-MESSAGE.strip_heredoc).to_stdout
-                .
-                ..
-                cat
-                cd
-                clear
-                core
-                echo
-                exit
-                ls
-                prompt
-                pwd
-                sys
-                wget
-                MESSAGE
+                # lib_files = Dir.entries('../../lib/packages')
+                # commands = lib_files.map{|command| command.include?('rb') ? command[0..-4] : command}
+                expect(Help.main('').to output('').to_stdout
             end
         end
     end
