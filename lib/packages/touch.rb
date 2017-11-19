89857862f4d4d710e8d5d2bc0cac1966be4f42d9
Parker Knight <pknight24@protonmail.com>
2017-11-19 16:38:31 -0500

    Restarting 'touch' command

--- lib/packages/touch.rb
+++ lib/packages/touch.rb
@@ -1,17 +1,5 @@
 class Touch
   def self.main params
-    fileExists = false
-    `ls`.each do |x|
-      if params[0] == x
-        fileExists = true
-      end
-    end
-
-    if fileExists?
-      puts "File already exists"
-    else
-      File.open(params[0], w)
-    end
+   puts "Touch test" 
   end
 end
-
