0a71908fc94ba6f90061616d70252c00eb1836ae
Parker Knight <pknight24@protonmail.com>
2017-11-23 16:31:09 -0500

    Use unless, Ls package

--- lib/packages/touch.rb
+++ lib/packages/touch.rb
@@ -1,12 +1,15 @@
 # simple implementation of the 'touch' command, which can be used to create empty text files
 # Initially, this can be used for simple file creation in shellrb. Eventually, I would like to allow for access time manipulation
+require_relative 'ls'
+
 class Touch
   def self.main params
     # user passes the name of the file to be created as the first parameter
     new_file = params[0]
-
+    
     # gets a list of the files and directories currently in the directory
-    ls = `ls`.split("\n")
+    
+    ls = Ls.show_hidden
     file_present = false
     
     # iterates through the files in the current directory to make sure that the desired file does not exist
@@ -17,11 +20,10 @@ class Touch
     end
 
     # creates the file if it does not exist already
-    # NOTE: I thought about showing an error message if the file already exists, but the originally touch command does not do this.
+    # NOTE: I thought about showing an error message if the file already exists, but the original touch command does not do this.
     # That can be added easily if deemed necessary.
-    if !file_present
-      File.write(new_file, "")
+    unless file_present
+      File.write(new_file, '')
     end
-
   end
 end
