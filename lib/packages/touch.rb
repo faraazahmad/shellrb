f9c46ee2e7cdc4aafae1dbdc48bf03089109ffef
Parker Knight <pknight24@protonmail.com>
2017-11-23 16:48:30 -0500

    Fix ls and rakefile

--- lib/packages/touch.rb
+++ lib/packages/touch.rb
@@ -1,22 +1,25 @@
 # simple implementation of the 'touch' command, which can be used to create empty text files
 # Initially, this can be used for simple file creation in shellrb. Eventually, I would like to allow for access time manipulation
 require_relative 'ls'
+require 'set'
 
 class Touch
   def self.main params
     # user passes the name of the file to be created as the first parameter
     new_file = params[0]
     
-    # gets a list of the files and directories currently in the directory
-    
-    ls = Ls.show_hidden
+    # creates a set of all the files in the current directory
+    ls = Set.new
+    ls.add Ls.show_hidden
+    ls.add Ls.show_non_hidden
+
     file_present = false
     
     # iterates through the files in the current directory to make sure that the desired file does not exist
     ls.each do |x|
-      if new_file == x
-          file_present = true
-      end
+      if x.include? new_file
+        file_present = true
+      end  
     end
 
     # creates the file if it does not exist already
@@ -25,5 +28,6 @@ class Touch
     unless file_present
       File.write(new_file, '')
     end
+
   end
 end
