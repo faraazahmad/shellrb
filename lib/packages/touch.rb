f3d0730994ac71a4e79b8122f831111793d8f1ad
Parker Knight <pknight24@protonmail.com>
2017-11-26 16:45:48 -0500

    Change iteration in touch, add break statement

--- lib/packages/touch.rb
+++ lib/packages/touch.rb
@@ -16,9 +16,13 @@ class Touch
     file_present = false
     
     # iterates through the files in the current directory to make sure that the desired file does not exist
-    ls.each do |x|
-      if x.include? new_file
-        file_present = true
+    # note that ls is a set of two sets, so we have to iterate through both sub-sets (see line 21)
+    ls.each do |set|
+      set.each do |x|
+        if x == new_file
+          file_present = true
+          break
+        end
       end  
     end
 
