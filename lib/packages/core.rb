6a5000603686f9958e67231262a2e07406a85350
Syed Faraaz Ahmad <faraaz98@live.com>
2017-11-27 18:39:08 +0530

    fix classes to pass tests

--- lib/packages/core.rb
+++ lib/packages/core.rb
@@ -4,16 +4,22 @@ class Core
 
   end
 
-  # def self.log_history command
-  # end
+  def self.init_history
+    $hist_file = File.open("#{Dir.home}/.srb_history", 'a+')
+  end
+
+  def self.log_history command
+    $hist_file.puts command
+  end
 
   # def self.load_profile
   # end
 
   def self.print_result result
     result.each do |value|
-      puts value
+      print "#{value}    "
     end
+    puts ''
   end
 
   def self.unknown_param param
