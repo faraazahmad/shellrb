462d3252893b3dccfa9bbeabc56b2d5459587d22
Parker Knight <pknight24@protonmail.com>
2017-12-01 07:55:30 -0500

    Touch command (#36)
    * Add basic cat command and error handling
    
    * Add comments to cat file
    
    * Add cat_spec file
    
    * Fix comment format
    
    * Fix cat spec file
    
    * Add test for echo.rb
    
    * Change to single quotes and fix whitespace
    
    * Start work on touch command
    
    * Restarting 'touch' command
    
    * Rm touch
    
    * Write touch comm, touch test
    
    * Use unless, Ls package
    
    * Update
    
    * Fix ls and rakefile
    
    * Change iteration in touch, add break statement
    
    * Change 'ls' to be combined set of Ls.show_hidden and Ls.show_non_hidden

---
+++ lib/packages/touch.rb
@@ -0,0 +1,32 @@
+# simple implementation of the 'touch' command, which can be used to create empty text files
+# Initially, this can be used for simple file creation in shellrb. Eventually, I would like to allow for access time manipulation
+require_relative 'ls'
+require 'set'
+
+class Touch
+  def self.main params
+    # user passes the name of the file to be created as the first parameter
+    new_file = params[0]
+    
+    # creates a set of all the files in the current directory
+    ls = Ls.show_hidden.merge Ls.show_non_hidden
+
+    file_present = false
+    
+    # iterates through the files in the current directory to make sure that the desired file does not exist
+    ls.each do |x|
+      if x == new_file
+        file_present = true
+        break
+      end  
+    end
+
+    # creates the file if it does not exist already
+    # NOTE: I thought about showing an error message if the file already exists, but the original touch command does not do this.
+    # That can be added easily if deemed necessary.
+    unless file_present
+      File.write(new_file, '')
+    end
+
+  end
+end
